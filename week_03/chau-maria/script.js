bcddea0b08cd93f858a68ef6e4426befd26a4b48
Maria <chau.marialuisa@gmail.com>
2014-10-02 20:22:49 -0400

    week3 work
    final

--- week_03/chau-maria/script.js
+++ week_03/chau-maria/script.js
@@ -1,21 +1,40 @@
 angular.module('studentApp', []).controller('StudentsController', ['$scope', function($scope) {
-	$scope.students = [
-		{index: 1, first_name: 'Calvin', last_name: 'Tennant', twitter_username: 'c11t_'},
-		{index: 2, first_name: 'Jason', last_name: 'McPherson', twitter_username: 'jmc_'},
-	];
+	//localStorage.setItem('students', 'testing');
+	console.log(localStorage.getItem('students'));
+	//return;
+
+	$scope.students = JSON.parse(localStorage.getItem('students'))  || [{
+		first_name: 'calvin', 
+		last_name: 'Tennant',
+		twitter_username: 'c11t_',
+		number: 1,
+	}];
 	
 	$scope.addStudent = function() {
 		console.log($scope.first_name);
 		$scope.students.push({
-			index: $scope.students.length + 1,
+			number: $scope.students.length + 1,
 			first_name: $scope.first_name, 
 			last_name: $scope.last_name, 
 			twitter_username: $scope.twitter_username,
 		});
+
 		$scope.first_name = '';
 		$scope.last_name = '';
 		$scope.twitter_username = '';
+
+		localStorage.setItem('students', JSON.stringify($scope.students));
 	}
+	$scope.removeStudent = function(student) {
+		var arrayIndex = $scope.students.indexOf(student);
+		$scope.students.splice(arrayIndex, 1);
 		
-	console.log($scope);
+		$scope.students.forEach(function(student, i) {
+			student.number = i + 1;
+			console.log(student, i);
+		});
+
+		localStorage.setItem('students', JSON.stringify($scope.students));
+
+	};
 }]); 
